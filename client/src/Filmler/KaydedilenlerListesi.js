17a8e9573f716d8d558a4bcf4cf4d3becb0bc832
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2023-12-03 14:24:38 +0300

    dosyalar düzenlendi

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/");
+  };
+
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
       {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <button className="home-button">Anasayfa</button>
+      <button onClick={handleClick} className="home-button">
+        Anasayfa
+      </button>
     </div>
   );
 }
